877757522a2faf395f09257dfdd3e855060f4b21
Leonardo Mallet <leonardoms.2010@hotmail.com>
2024-04-26 18:37:28 -0300

    Mudança na Main
    Segundo teste

---
+++ Visitantes.java
@@ -0,0 +1,13 @@
+public class Visitantes {
+    private String nome;
+    private int anoNascimento;
+    private int telefone;
+
+    public Visitantes(String nome, int anoNascimento, int telefone){
+        this.nome = nome;
+        this.anoNascimento = anoNascimento;
+        this.telefone = telefone;
+    }
+
+    
+}
