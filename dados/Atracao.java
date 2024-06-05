dcad9346ba40ae8b288b8e31bd8ff0323261776a
Leonardo Mallet <leonardoms.2010@hotmail.com>
2024-06-05 19:04:31 -0300

    Criação das Classes Bases pt2 + Definição de 2 outras classes
    Com base no Diagrama de Classes feito no Astah, foi feita a criação das seguintes classes de objetos que usaremos como base para desenvolver o Projeto do Parque de Diversão:
    
    -Ingresso
    -Atracao
    
    Além disso, foi definido 2 classes que também usaremos para desenvolver o Projeto do Parque de Diversão, porém não foi desenvolvido nada nessas duas classes que são:
    
    -ParqueDeDiversao(Aplicação)
    -Bilheteria

---
+++ dados/Atracao.java
@@ -0,0 +1,22 @@
+package dados;
+public class Atracao {
+    private String nome;
+    private int visitas;
+    
+    public Atracao(String nome) {
+        this.nome = nome;
+        this.visitas = 0;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getVisitas() {
+        return visitas;
+    }
+
+    public void visitado() {
+        this.visitas++;
+    }
+}
