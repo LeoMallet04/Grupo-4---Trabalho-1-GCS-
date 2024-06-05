62049c0cb53f73fe3c557685c1843cbd7f3d5ba0
Leonardo Mallet <leonardoms.2010@hotmail.com>
2024-06-05 15:54:17 -0300

    Criação das Classes Bases pt 1
    Com base no Diagrama de Classes feito no Astah, foi feito a criação das seguintes classes de objetos que usaremos como base para desenvolver o Projeto do Parque de Diversão:
    
    -Adulto
    -Crianca
    -Pessoa

---
+++ dados/Adulto.java
@@ -0,0 +1,13 @@
+package dados;
+public class Adulto extends Pessoa {
+    private int telefone;
+
+    public Adulto(String nome, int anoNascimento, int telefone) {
+        super(nome, anoNascimento);
+        this.telefone = telefone;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+}
